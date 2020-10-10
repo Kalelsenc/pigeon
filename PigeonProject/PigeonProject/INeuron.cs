a590bd1b570c3435effe6f5cccfda0b9ed4d59f1
Игорь Шадрин <igor@shadrins.ru>
2020-10-11 00:48:23 +0300

    Оформление
    Описание того, что нужно сделать и как это делать

---
+++ PigeonProject/PigeonProject/INeuron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigeonProject
+{
+    interface INeuron<T, E>
+    {
+        void push(T value);
+        E get();
+    }
+}
