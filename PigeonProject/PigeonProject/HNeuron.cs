e39fe25708826ae777b9c436f52361ecd2be0d35
Арсений <nikolaychuk_a@mail.ru>
2020-10-13 16:51:36 +0300

    HNeuron

---
+++ PigeonProject/PigeonProject/HNeuron.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigeonProject
+{
+    class HNeuron : INeuron<double, double>
+    {
+        double sum = 0;
+
+        public void clear()
+        {
+            sum = 0;
+        }
+
+        public double get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void push(double value)
+        {
+            sum = sum + value;
+        }
+    }
+}
